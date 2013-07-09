60c9695c9434a9077f26bf0c036ff8c133a5e45d
Ubuntu <ubuntu@ip-172-31-40-193.us-west-2.compute.internal>
2013-07-09 04:55:34 +0000

    small fix

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 var buf = fs.readFileSync('index.html', 'utf8');
 app.get('/', function(request, response) {
-  response.send(buf.toString("utf-8");
+  response.send(buf.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
